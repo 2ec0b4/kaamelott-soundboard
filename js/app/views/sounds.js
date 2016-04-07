4eecf7bd544218597e2685d7541e72e36672b2ef
Antoine <antoine@2ec0b4.fr>
2016-04-07 17:52:13 +0200

    Récupère le contenu du fichier JSON et l'affiche

--- js/app/views/sounds.js
+++ js/app/views/sounds.js
@@ -2,13 +2,29 @@ define(
     'views/sounds',
     [
         'marionette',
+        'jquery',
         'hbs!templates/sounds'
     ],
-    function (Marionette, SoundListTemplate) {
+    function (Marionette, $, SoundListTemplate) {
         "use strict";
 
         var SoundListView = Marionette.LayoutView.extend({
-            template: SoundListTemplate
+            template: SoundListTemplate,
+            initialize: function() {
+                var that    = this;
+
+                this.data = {};
+
+                $.get('/sounds/sounds.json', function(json) {
+                    that.data.sounds    = json;
+                    that.render();
+                });
+            },
+            serializeData: function () {
+                var viewData = {data: this.data};
+
+                return _.extend(viewData, Marionette.LayoutView.prototype.serializeData.apply(this, arguments));
+            }
         });
 
         return SoundListView;
