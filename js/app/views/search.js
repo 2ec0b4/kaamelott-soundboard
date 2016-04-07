0c84cd6968db551c9c8dd4fe93dc4d84c631567b
Antoine <antoine@2ec0b4.fr>
2016-04-07 17:52:30 +0200

    Initialise la recherche

--- js/app/views/search.js
+++ js/app/views/search.js
@@ -8,7 +8,18 @@ define(
         "use strict";
 
         var SoundSearchView = Marionette.LayoutView.extend({
-            template: SoundSearchTemplate
+            template: SoundSearchTemplate,
+            ui: {
+                searchForm:'form',
+                searchField:'form input[name="s"]',
+            },
+            events: {
+                'submit @ui.searchForm':'filterSounds',
+                'keyup @ui.searchField':'filterSounds'
+            },
+            filterSounds: function() {
+                var search  = $(this.ui.searchField).val();
+            }
         });
 
         return SoundSearchView;
