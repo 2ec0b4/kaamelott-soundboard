050a8dd79dac0ed44fa6b3bd4b3c80ef0936dd6c
Antoine <antoine@2ec0b4.fr>
2016-05-31 23:44:12 +0200

    Autres build tests ave Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-var gulp = require('gulp');
-var rev = require('gulp-rev');
+var gulp = require('gulp'),
+    rev = require('gulp-rev'),
+    revReplace = require('gulp-rev-replace'),
+    uglify = require('gulp-uglify');
 
-gulp.task('default', function () {
-    return gulp.src(['js/app/*.js', 'js/app/*/*.js'])
-        .pipe(rev())
-        .pipe(gulp.dest('dist'));
+gulp.task("revision", function(){
+  return gulp.src(['js/app/*.js', 'js/app/*/*.js'])
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('dist'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task("revreplace", function(){
+  var manifest = gulp.src("./dist/rev-manifest.json");
+
+  return gulp.src(['dist/*.js', 'dist/*/*.js'])
+    .pipe(revReplace({manifest: manifest}))
+    .pipe(gulp.dest('dist'));
 });
