ccbbf8743d9538d346adc216e5c3612a6233280c
Antoine <s.github@2ec0b4.fr>
2018-10-10 22:34:38 +0200

    Corrige une erreur sur l'autoplay pour Google Chrome

--- js/app/models/sound.js
+++ js/app/models/sound.js
@@ -19,11 +19,20 @@ define("models/sound",  function(require) {
                 this.audio = new Audio("sounds/"+this.get("file"));
             }
 
-            this.audio.play();
+            var that    = this,
+                promise = this.audio.play();
+
+            if (promise !== undefined) {
+                promise.then(function(_) {
+                    that.set("playing", true);
+                }).catch(function(error) {
+                });
+            } else {
+                this.set("playing", true);
+            }
+
             this.audio.onended = this.stop.bind(this);
             this.audio.onpause = this.stop.bind(this);
-
-            this.set("playing", true);
         },
         stop: function() {
             if( this.audio && !this.audio.paused ) {
